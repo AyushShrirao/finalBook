46a7f25b7baff1366747ee942815edaa53bdb93f
Ayush Shrirao <ayushshrirao16@gmail.com>
2025-01-04 20:53:09 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("/user/signup", userInfo)
+      .post("https://finalbook.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
